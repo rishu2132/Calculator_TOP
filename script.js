4e8bccec717b32c820e0ed5cdf94be7fe184a189
utkarsh <utk1298@gmail.com>
2026-01-15 01:18:05 +0530

    make the structure of calculator and function

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function division(a,b){
+    return a/b
+}
+
+function operate(op,a,b){
+    return op(a,b)
+}
